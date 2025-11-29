711f50e196db2aedd0c892e176339d19938595af
muxlisa0206 <sayfullayevamuxlisa27@gmail.com>
2025-11-29 10:30:20 +0500

    responsivlik header va foote

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.svg';
 import vk from '../../assets/vk.png';
 import pinterest from '../../assets/pinterest.png';
 import tg from '../../assets/tg.png';
@@ -8,55 +8,49 @@ import { NavLink } from 'react-router-dom'
 
 function Footer() {
   return (
-    <footer className='bg-[#3B3937]'>
-      <div className='container mx-auto px-[20px] flex justify-between gap-[10px]'>
-        <div className='box flex items-center justify-between gap-[10px] px-[30px] pb-[50px] pt-[30px] max-w-[660px] w-full '
-        style={{
-          backgroundImage: `url(${bg})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}>
-          <div className='flex flex-col gap-[15px] justify-center'>
+    <footer className='mt-[150px]'
+    style={{
+      backgroundImage: `url(${bg})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }}>
+      <div className=' bg-black/50'>
+        <div className='container mx-auto px-[30px] py-[50px] flex flex-col items-center text-center sm:text-left sm:flex-row flex-wrap justify-between gap-[50px]'>
+          <div className='flex flex-col gap-[15px]'>
             <NavLink to="/"><img src={logo} alt="" /></NavLink>
-            <p className='font-[400] text-[13px] text-white'>Фабрика G&G. 2022</p>
-            <p className='font-[400] text-[13px] text-white'>© Все права защищены</p>
-          </div>
-          <div className='flex flex-col gap-[20px] justify-center'>
-            <p className='text-white text-[25px] font-[700]'>8 (800) 550-81-79</p>
-            <div className='flex flex-col gap-[10px]'>
-              <p className='text-white text-[13px] font-[400]'>м. Полежаевская</p>
-              <p className='max-w-[245px] w-full text-white text-[13px] font-[400]'>123154, г. Москва, пр-т. Маршала Жукова, д. 52, "Мебельный Центр"</p>
-              <p className='text-white text-[13px] font-[400]'>support@sofiadoors.com</p>
+            <div className='flex flex-col gap-[8px]'>
+              <p className='text-white text-[13px] font-[400]'>Фабрика G&G. 2022</p>
+              <p className='text-white text-[13px] font-[400]'>© Все права защищены</p>
             </div>
+         </div>
+         <div className='flex flex-col gap-[25px]'>
+          <h1 className='text-white text-[25px] font-bold'>8 (800) 550-81-79</h1>
+          <div className='flex flex-col gap-[15px]'>
+            <p className='text-white text-[13px] font-[400]'>м. Полежаевская</p>
+            <p className='text-white text-[13px] font-[400] max-w-[245px] w-full'>123154, г. Москва, пр-т. Маршала Жукова, д. 52, "Мебельный Центр"</p>
+            <p className='text-white text-[13px] font-[400]'>support@sofiadoors.com</p>
           </div>
-        </div>
-        <div className='flex flex-col gap-[15px] mt-[20px]  pb-[50px] text-center max-w-[550px] w-full'>
-          <p className='text-white text-[13px] font-[400]'>Межкомнатные двери</p>          <p className='text-white text-[13px] font-[400]'>Межкомнатные двери</p>
+         </div>
+         <div className='flex flex-col gap-[15px]'>
+          <p className='text-white text-[13px] font-[400]'>Межкомнатные двери</p>
           <p className='text-white text-[13px] font-[400]'>Мебель</p>
           <p className='text-white text-[13px] font-[400]'>Стеновые панели «Буазери»</p>
           <p className='text-white text-[13px] font-[400]'>Лестницы</p>
           <p className='text-white text-[13px] font-[400]'>Мебельные фасады</p>
-        </div>
-        <div className='box flex flex-col gap-[35px] px-[30px] pb-[50px] pt-[50px] max-w-[660px] w-full '
-        style={{
-          backgroundImage: `url(${bg})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}>
-            <div className='flex justify-between'>
-              <div className='flex flex-col gap-[13px]'>
-                <NavLink className="text-white text-[13px] font-[400]" to="Contact">Contact</NavLink>
-                <NavLink className="text-white text-[13px] font-[400]" to="State">State</NavLink>
-                <NavLink className="text-white text-[13px] font-[400]" to="Dastavka">Dastavka</NavLink>
-
+         </div>
+         <div className='flex flex-col gap-[20px] lg:flex-row items-start justify-between'>
+              <div className='flex flex-col gap-[15px]'>
+                <NavLink to='Dastavka' className='text-white text-[13px] font-[400]'>Dastavka</NavLink>
+                <NavLink to='State' className='text-white text-[13px] font-[400]'>State</NavLink>
+                <NavLink to='Contact' className='text-white text-[13px] font-[400]'>Contact</NavLink>
+                <p className='text-white text-[13px] font-[400]'>Политика конфиденциальности</p>
               </div>
               <div className='flex items-center'>
-                <div className='w-[50px] h-[50px] rounded-[50%] border-[#505050] border-1 bg-[#3B3937] flex items-center justify-center'><img src={vk} alt="" /></div>
-                <div className='w-[50px] h-[50px] rounded-[50%] border-[#505050] border-1 bg-[#3B3937] flex items-center justify-center'><img src={pinterest} alt="" /></div>
-                <div className='w-[50px] h-[50px] rounded-[50%] border-[#505050] border-1 bg-[#3B3937] flex items-center justify-center'><img src={tg} alt="" /></div>
+                <div className='border-1 bg-[#3B3937] border-[#505050] w-[40px] h-[40px] rounded-[50%] flex justify-center items-center'><img className='w-[30px] h-[30px]' src={vk} alt="" /></div>
+                <div className='border-1 bg-[#3B3937] border-[#505050] w-[40px] h-[40px] rounded-[50%] flex justify-center items-center ml-[-10px]'><img className='w-[30px] h-[30px]' src={pinterest} alt="" /></div>
+                <div className='border-1 bg-[#3B3937] border-[#505050] w-[40px] h-[40px] rounded-[50%] flex justify-center items-center ml-[-10px]'><img className='w-[30px] h-[30px]' src={tg} alt="" /></div>
               </div>
-            </div>
-            <p className="text-white text-[13px] font-[400]">Политика конфиденциальности</p>
+          </div>
         </div>
       </div>
     </footer>
--- src/page/home/HomePage.jsx
+++ src/page/home/HomePage.jsx
@@ -4,13 +4,20 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/zoom';
-import 'swiper/css/navigation';
+import 'swiper/css/effect-flip';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { NavLink } from 'react-router-dom'
+
+
 // import required modules
-import { Zoom, Navigation, Pagination } from 'swiper/modules';
+import { EffectFlip, Pagination, Navigation } from 'swiper/modules';
+
+import './homePage.css'
 
 import footerbg from '../../assets/footerbg.png';
+import image1 from '../../assets/image1.jpg';
+import image2 from '../../assets/image2.png';
 
 const HomePage = () => {
   return (
@@ -19,19 +26,28 @@ const HomePage = () => {
         <section  className='h-[100vh] m-[20px] mt-[130px] bg-[url(herobg.jpg)]'
        >
          <Swiper
-            style={{
-              '--swiper-navigation-color': '#fff',
-              '--swiper-pagination-color': '#fff',
-            }}
-            zoom={true}
-            navigation={true}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Zoom, Navigation, Pagination]}
-            className="mySwiper h-full flex items-center justify-center"
-          >
-             <SwiperSlide className='flex items-center justify-center'>
+        effect={'flip'}
+        grabCursor={true}
+        pagination={true}
+        navigation={true}
+        modules={[EffectFlip, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide className='flex items-center justify-center relative'>
+              <div className='container mx-auto px-[30px] flex flex-col gap-[10px] items-center justify-center max-w-[540px] w-full h-[540px] rounded-[50%]' 
+              style={{
+                backgroundImage: `url(${footerbg})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
+              <h1 className='text-[50px] text-white font-bold'>Classic Wood Stile</h1>
+              <p className='text-[#FFD6A8] text-[35px] font-bold'>enterior-exterior</p>
+              <p className='text-center w-full text-white text-[20px] font-bold'>Приглашает к сотрудничеству архитекторов, дизайнеров и строителей</p>
+              <button className='px-[25px] py-[15px] bg-[#A9845C] shadow-[1px_1px_1px_0px_#00000040] text-white font-bold text-[20px] uppercase mt-[30px]'>подробнее</button>
+            </div>
+            </SwiperSlide>
+            <SwiperSlide className='flex items-center justify-center relative'>
               <div className='container mx-auto px-[30px] flex flex-col gap-[10px] items-center justify-center max-w-[540px] w-full h-[540px] rounded-[50%] opacity-50' 
               style={{
                 backgroundImage: `url(${footerbg})`,
@@ -45,7 +61,7 @@ const HomePage = () => {
               <button className='px-[25px] py-[15px] bg-[#A9845C] shadow-[1px_1px_1px_0px_#00000040] text-white font-bold text-[20px] uppercase mt-[30px]'>подробнее</button>
             </div>
             </SwiperSlide>
-             <SwiperSlide className='flex items-center justify-center'>
+            <SwiperSlide className='flex items-center justify-center relative'>
               <div className='container mx-auto px-[30px] flex flex-col gap-[10px] items-center justify-center max-w-[540px] w-full h-[540px] rounded-[50%] opacity-50' 
               style={{
                 backgroundImage: `url(${footerbg})`,
@@ -59,21 +75,28 @@ const HomePage = () => {
               <button className='px-[25px] py-[15px] bg-[#A9845C] shadow-[1px_1px_1px_0px_#00000040] text-white font-bold text-[20px] uppercase mt-[30px]'>подробнее</button>
             </div>
             </SwiperSlide>
-             <SwiperSlide className='flex items-center justify-center'>
-              <div className='container mx-auto px-[30px] flex flex-col gap-[10px] items-center justify-center max-w-[540px] w-full h-[540px] rounded-[50%] opacity-50' 
-              style={{
-                backgroundImage: `url(${footerbg})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-              }}
-            >
-              <h1 className='text-[50px] text-white font-bold'>Classic Wood Stile</h1>
-              <p className='text-[#FFD6A8] text-[35px] font-bold'>enterior-exterior</p>
-              <p className='text-center w-full text-white text-[20px] font-bold'>Приглашает к сотрудничеству архитекторов, дизайнеров и строителей</p>
-              <button className='px-[25px] py-[15px] bg-[#A9845C] shadow-[1px_1px_1px_0px_#00000040] text-white font-bold text-[20px] uppercase mt-[30px]'>подробнее</button>
-            </div>
-            </SwiperSlide>
-          </Swiper>
+      </Swiper>
+        </section>
+
+        <section>
+          <div className='container mx-auto px-[30px] pt-[40px] flex items-center justify-between gap-[20px]'>
+              <div className='max-w-[700px] w-full border-b-1 border-[#DADADA]'>
+                <img className='w-full h-[320px] object-cover' src={image1} alt="" />
+                <div className='py-[20px] flex flex-col gap-[20px]'>
+                  <p className='text-[#3B3937] text-[36px] font-bold'>Двери на заказ</p>
+                  <p className='text-[#3B3937] text-[16px] font-[400] max-w-[545px] w-full'>На собственной фабрике мы создаем красивые, безопасные и практически вечные двери, межкомнатные перегородки, стеновые панели и другие решения для вашего интерьера.</p>
+                  <NavLink to='Contact' className='text-right text-[#B14101] text-[15px] font-bold'>заказать уникальную дверь</NavLink>
+                </div>
+              </div>
+              <div className='max-w-[700px] w-full border-b-1 border-[#DADADA]'>
+                <img className='w-full h-[320px] object-cover' src={image2} alt="" />
+                <div className='py-[20px] flex flex-col gap-[20px]'>
+                  <p className='text-[#3B3937] text-[36px] font-bold'>3D фрезеровка</p>
+                  <p className='text-[#3B3937] text-[16px] font-[400] max-w-[545px] w-full'>Изделия, которые создает фрезерный станок, управляемый компьютером, отличаются высочайшим качеством, абсолютной детализацией и идеальной точностью.</p>
+                  <NavLink to='Catalog' className='text-right text-[#B14101] text-[15px] font-bold'>выбрать из каталога</NavLink>
+                </div>
+              </div>
+          </div>
         </section>
       </main>
     </div>
