8dbe6458f5c3e4af853c0df8481a4859c3120a5b
frontend <husaboyevoyatbek@gmail.com>
2025-12-01 21:20:42 +0500

    HAritagaOzgarishQoshdm

--- src/page/contact/ContactPage.jsx
+++ src/page/contact/ContactPage.jsx
@@ -1,3 +1,4 @@
+import { Circle, GeolocationControl, Map, Placemark, TrafficControl, TypeSelector, YMaps, ZoomControl } from '@pbe/react-yandex-maps'
 import React from 'react'
 
 const ContactPage = () => {
@@ -70,7 +71,7 @@ const ContactPage = () => {
                     8 (900) 000-00-00
                     <span className='mx-3'></span>
                     8 (900) 000-00-00
-                  </h1>
+                  </h1> 
                   <br />
 
                   <p className=' text-[13px] text-[#3B3937]'>
@@ -96,13 +97,52 @@ const ContactPage = () => {
 
 
               <div id='map' className="w-full max-w-[730px] map">
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2523.4478036367773!2d69.26924483256072!3d41.2239042649056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae61bf92c68405%3A0x13b3bd3727f6fbc7!2sAlfa%20Mebel!5e0!3m2!1sru!2s!4v1764180948111!5m2!1sru!2s"
-                    className="w-full h-[600px] border-0"
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                  ></iframe>
+
+
+                <YMaps>
+                  <Map className='w-full h-full' defaultState={{ center: [41.2646, 69.2163], zoom: 15 }}>
+                    <Placemark
+                      geometry={[41.2646, 69.2163]}
+                      options={{
+                        iconLayout: "default#image",
+                        iconImageHref: "/contactImg/location-icon.png", 
+                        iconImageSize: [40, 40], 
+                        iconImageOffset: [-20, -20], 
+                      }}
+                    />
+
+
+                    <TrafficControl options={{ float: "right" }} />
+                    <TypeSelector options={{ float: "right" }} />
+
+
+                    <ZoomControl options={{ float: "right", position: {
+                      left: 10,
+                    top:200
+                    } }} />
+
+                    <Circle
+                      geometry={[[41.2646, 69.2163], 100]}
+                      options={{
+                        draggable: true,
+                        fillColor: "rgba(139, 69, 19, 0.25)",   
+                        strokeColor: "#8B4513",                
+                        strokeOpacity: 0.9,
+                        strokeWidth: 4,
+                        strokeStyle: "solid",
+                      }}
+                    />
+
+
+                    <GeolocationControl options={{ float: "left" }} />
+
+
+
+                  </Map>
+
+
+                  
+                </YMaps>
                 </div>
 
 
